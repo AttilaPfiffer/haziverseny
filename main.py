fb7a7ea4c56f1fa55fe412b87fe379a28d20ca86
AttilaPfiffer <pifuati@gmail.com>
2023-12-13 13:18:45 +0100

    epulet

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print ("Utad során különböző helyszínekre kerülsz, ahonnan szabadon átmehetsz másik helyszínre, a megadott módon. Akár ide-oda is mászkálhatsz, bár ennek meglesz a következménye, ld: technikai leírásnál! Az adott helyszínen megvizsgálhatsz tárgyakat, fel is veheted őket vagy akár használhatod őket. Ha egy helyszínen felvettél egy tárgyat, akkor az legközelebb már nincs ott! Ha felvettél egy tárgyat, akkor és csak akkor van lehetőséged használni, ha erre kiadod a megfelelő parancsot. Ez lehet a kulcsa a továbbjutásnak. Amennyiben rossz helyen használsz egy tárgyat, akkor az a tárgy nem veszik el, csak nem történik változás a történet menetében.")
+
+print("Az életerő: A harmadik helyszínváltás után elkezd csökkeni az életerő, helyszínenként eggyel! Erről tájékoztatni kell a játékost! Kezdetben az életereje 5, ha talál ételt és megeszi (használja), akkor ismét 3 helyszínváltásig nem csökken, és maximumomra (10) kerül. Majd újra elkezd csökkenni helyszínenként eggyel. Ha nullára csökken, akkor vége a játéknak, nincs erőnk továbbmenni…")
+
+
+
+
+enter = input("A játék kezdéséhez nyomjon egy ENTER-t!")
+
+
+epulet: str = str(input("Egy óriási mezőn vagy. Nyugat felé egy hatalmas épület körvonalai tűnnek fel."))
+if "megy epulet" or "Megy epulet" or "megy épület" or "Megy épület" in epulet:
+    
+
